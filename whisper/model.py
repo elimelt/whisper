e764458c2d51ef87e12fb02a972996f68c091579
Deeksha Vatwani <deekshavatwani@Deekshas-MacBook-Air-2.local>
2025-02-18 14:51:39 -0800

    add debug prints in prune

--- whisper/model.py
+++ whisper/model.py
@@ -189,11 +189,12 @@ class AudioEncoderTokenPruner():
 
     def prune(self, x: Tensor, positional_embedding: Tensor, token_count: int):
         if token_count != -1:
-            cut_region = [ token_count, TOTAL_NUM_TOKENS - 200 ]
+            self.cut_region = [ token_count, TOTAL_NUM_TOKENS - 200 ]
 
             # audio_length = int((x.shape[1] + 1) // 2)
             # [0-950, -----, 1300-1500]
-
+        print('num tokens: ', token_count )
+        print('new cut region: ', self.cut_region )
         cut_start, cut_end = self.cut_region
         assert 0 <= cut_start < cut_end <= x.shape[1], "Cut region out of bounds!"
 
